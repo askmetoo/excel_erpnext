41e1e416a0daca8e6300faa51d78a0f1142aab09
Revant Nandgaonkar <revant.one@gmail.com>
2020-08-09 00:43:10 +0530

    wip: queue background log one by one, fix indent

--- excel_erpnext/schedules/purchase_receipt.py
+++ excel_erpnext/schedules/purchase_receipt.py
@@ -4,60 +4,60 @@ def process_pr():
 	d.status = 'Fail'
 	d.save()
 	frappe.db.commit()
-    statuses = frappe.get_list(
-        "Excel Background Log",
-        fields=["status"],
-        filters={"status":"In progress"}
-    )
+	statuses = frappe.get_list(
+		"Excel Background Log",
+		fields=["status"],
+		filters={"status":"In progress"}
+	)
 
-    if len(statuses) > 0:
-        return
+	if len(statuses) > 0:
+		return
 
-    background_logs = frappe.get_list(
-        "Excel Background Job",
-        filters={"status":"Queued"},
-        order_by='creation asc'
-    )
+	background_logs = frappe.get_list(
+		"Excel Background Job",
+		filters={"status":"Queued"},
+		order_by='creation asc'
+	)
 
-    if len(background_logs) > 0:
-        background_log = frappe.get_doc(
-            "Excel Background Job",
-            background_logs[0].get('name')
-        )
+	if len(background_logs) > 0:
+		background_log = frappe.get_doc(
+			"Excel Background Job",
+			background_logs[0].get('name')
+		)
 
-        json_doc = json.loads(background_log.body)
-        background_log.status = "In progress"
-        background_log.save()
-        frappe.db.commit()
+		json_doc = json.loads(background_log.body)
+		background_log.status = "In progress"
+		background_log.save()
+		frappe.db.commit()
 
-        try:
-        	# Save Purchase Receipt
-        	doc = frappe.get_doc(frappe._dict(json_doc))
-        	doc.save()
-        	frappe.db.commit()
+		try:
+			# Save Purchase Receipt
+			doc = frappe.get_doc(frappe._dict(json_doc))
+			doc.save()
+			frappe.db.commit()
 
-        	# Submit Purchase Receipt
-        	doc.submit()
-        	frappe.db.commit()
+			# Submit Purchase Receipt
+			doc.submit()
+			frappe.db.commit()
 
-        	# Save success to custom doctype
-        	background_log.success_log = doc.get('name')
-        	background_log.status = "Complete"
-        	background_log.save()
-        	frappe.db.commit()
+			# Save success to custom doctype
+			background_log.success_log = doc.get('name')
+			background_log.status = "Complete"
+			background_log.save()
+			frappe.db.commit()
 
-        except Exception as exc:
-        	# Delete draft doc
-        	if (
-        		doc
-        		and doc.docstatus == 0
-        		and not doc.get('__islocal')
-        	):
-        		doc.delete()
-        		frappe.db.commit()
+		except Exception as exc:
+			# Delete draft doc
+			if (
+				doc
+				and doc.docstatus == 0
+				and not doc.get('__islocal')
+			):
+				doc.delete()
+				frappe.db.commit()
 
-        	# Save error to custom doctype
-        	background_log.error_log = repr(exc)
-        	background_log.status = "Fail"
-        	background_log.save()
-        	frappe.db.commit()
+			# Save error to custom doctype
+			background_log.error_log = repr(exc)
+			background_log.status = "Fail"
+			background_log.save()
+			frappe.db.commit()

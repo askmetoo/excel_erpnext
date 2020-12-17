64355c782ac266de3a6b13298e1f1cccc3ca3b30
Revant Nandgaonkar <revant.one@gmail.com>
2020-12-17 13:16:10 +0530

    fix: po update query

--- excel_erpnext/schedules/purchase.py
+++ excel_erpnext/schedules/purchase.py
@@ -2,25 +2,23 @@ import frappe
 
 
 def process_purchase_orders():
-    purchase_orders = frappe.get_all(
-        "Purchase Order",
-        filters = [
-            ["per_received", "<", 100],
-        ],
-    )
-
-    for po in purchase_orders:
-        if po.get('name'):
-            frappe.db.sql(
-                '''
-                UPDATE `tabPurchase Order` po
-                    INNER JOIN (
-                        SELECT purchase_order, SUM(qty) as total
-                        FROM `tabPurchase Receipt Item`
-                        GROUP BY purchase_order
-                    ) pr ON po.name = pr.purchase_order
-                    SET po.per_received = pr.total * 100 / po.total_qty
-                    WHERE po.name = "%s";
-                ''',
-                po.get('name'),
-            )
+    try:
+        frappe.db.begin()
+        frappe.db.sql(
+            '''
+            UPDATE `tabPurchase Order` po
+                INNER JOIN (
+                    SELECT purchase_order, SUM(qty) as total
+                    FROM `tabPurchase Receipt Item`
+                    GROUP BY purchase_order
+                ) pr ON po.name = pr.purchase_order
+                SET po.per_received = pr.total * 100 / po.total_qty
+                WHERE po.status NOT IN ("Completed","Cancelled") AND po.per_received < 100;
+            ''',
+        )
+    except Exception as e:
+        frappe.db.rollback()
+    else:
+        frappe.db.commit()
+    finally:
+        frappe.destroy()

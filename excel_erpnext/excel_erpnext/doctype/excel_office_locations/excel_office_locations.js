d66bb87b38667a125507a8ba0e11f3a0dac8a8b5
Farid Ahmed <fnrfarid@gmail.com>
2021-01-28 06:49:08 +0000

    feat: added several doctypes, print formats, and more custom fields. There will be changes later to these doctypes.

---
+++ excel_erpnext/excel_erpnext/doctype/excel_office_locations/excel_office_locations.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Castlecraft Ecommerce Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Excel Office Locations', {
+	// refresh: function(frm) {
+
+	// }
+});

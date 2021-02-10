949f7ece2e95e1da96afa8db655e980b032ff766
Farid Ahmed <fnrfarid@gmail.com>
2021-02-10 08:43:50 +0000

    feat: fix tax_payment custom field to excel_tax_payment

--- excel_erpnext/excel_erpnext/report/excel_company_performance_summary/excel_company_performance_summary.py
+++ excel_erpnext/excel_erpnext/report/excel_company_performance_summary/excel_company_performance_summary.py
@@ -72,7 +72,7 @@ def get_data(filters=None):
 			collection_amount = frappe.db.sql(""" SELECT
                                 sum(paid_amount)
                                 FROM `tabPayment Entry`
-                                where docstatus = 1 and posting_date >= %s and posting_date <= %s and party = %s and tax_payment = "No" and payment_type = "Receive"
+                                where docstatus = 1 and posting_date >= %s and posting_date <= %s and party = %s and excel_tax_payment = "No" and payment_type = "Receive"
                                 """,(filters.get('from_date'),filters.get('to_date'),str(i['name'])))
 			if collection_amount[0][0]:
 				collection += collection_amount[0][0]
@@ -82,7 +82,7 @@ def get_data(filters=None):
 		collection_amount = frappe.db.sql(""" SELECT
                                 sum(paid_amount)
                                 FROM `tabPayment Entry`
-                                where docstatus = 1 and posting_date >= %s and posting_date <= %s and tax_payment = "No" and payment_type = "Receive"
+                                where docstatus = 1 and posting_date >= %s and posting_date <= %s and excel_tax_payment = "No" and payment_type = "Receive"
                                 """,(filters.get('from_date'),filters.get('to_date')))
 		collection  = collection_amount[0][0]
 	row = {
@@ -230,7 +230,7 @@ def whole_net_sales_conditions(filters):
 def collection_conditions(filters):
 	conditions = ""
 	customer= []
-	conditions += "docstatus = 1 and tax_payment = 'No' and payment_type = 'Receive' "
+	conditions += "docstatus = 1 and excel_tax_payment = 'No' and payment_type = 'Receive' "
 	if filters.get("customer_group") and filters.get("territory"):
 		customer = frappe.db.sql(""" SELECT
                 name

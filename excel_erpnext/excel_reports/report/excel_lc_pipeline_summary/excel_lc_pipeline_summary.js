0a83cf149fdc663aa76f72116c3ef9edc4c25ae6
Farid Ahmed <fnrfarid@gmail.com>
2021-03-22 12:46:52 +0000

    fix: pipeline report filter

--- excel_erpnext/excel_reports/report/excel_lc_pipeline_summary/excel_lc_pipeline_summary.js
+++ excel_erpnext/excel_reports/report/excel_lc_pipeline_summary/excel_lc_pipeline_summary.js
@@ -2,14 +2,3 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Excel LC Pipeline Summary"] = {
-	"filters": [
-		{
-			"fieldname":"pipeline_status",
-			"label": __("Pipeline Status"),
-			"fieldtype": "Select",
-			"options": "Complete\nIn Progress",
-            "default": "In Progress"
-		},
-	]
-}
